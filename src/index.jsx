44d1965c496e67efb626b8b408be7d0879a845ea
Joanne <yae.joanne@gmail.com>
2016-03-14 19:11:24 -0400

    action creators

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,65 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Router, Route, hashHistory} from 'react-router';
+// import {createStore, applyMiddleware} from 'redux';
+// import reducer from './reducer';
+// import {setState} from './action_creators';
+// import remoteActionMiddleware from './remote_action_middleware';
+// import {Provider} from 'react-redux';
+// import io from 'socket.io-client';
+// import App from './components/App';
+// import {VotingContainer} from './components/Voting';
+// import {ResultsContainer} from './components/Results';
+
+// const socket = io(`${location.protocol}//${location.hostname}:8090`);
+// socket.on('state', state =>
+//   store.dispatch(setState(state))
+// );
+
+// const createStoreWithMiddleware = applyMiddleware(
+//   remoteActionMiddleware(socket)
+// )(createStore);
+// const store = createStoreWithMiddleware(reducer);
+
+
+
+// const routes =
+// <Route component={App}>
+//   <Route path="/results" component={ResultsContainer} />
+//   <Route path="/" component={VotingContainer} />
+// </Route>;
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={hashHistory}>{routes}</Router>
+//   </Provider>,
+//   document.getElementById('app')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {createStore} from 'redux';
-import reducer from './reducer';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
-const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
-
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
-socket.on('state', state => {
-  store.dispatch({type: 'SET_STATE', state});
-  console.log('STATE', state);
-});
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
 
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
-const pair = ['Trainspotting', '28 Days Later'];
-
-const routes =
-<Route component={App}>
+const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
   <Route path="/" component={VotingContainer} />
 </Route>;
